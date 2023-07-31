125c7ad8faa22b1d432e1b50b7bbeabac8f2e6cc
alyfer-pedroso <alyferleandroaraujopedroso@gmail.com>
2023-07-31 20:03:18 -0300

    edit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-toLoginPage = () => {
-    const $loading = document.querySelector(".loading");
-    const $content = document.querySelector(".content");
+// toLoginPage = () => {
+//     const $loading = document.querySelector(".loading");
+//     const $content = document.querySelector(".content");
 
-    setTimeout(() => {
-        $content.style.display = "flex";
-        $loading.style.opacity = 0;
-        setTimeout(() => {
-            $loading.style.display = "none";
-        }, 1000);
-    }, 6000);
-};
-toLoginPage();
+//     setTimeout(() => {
+//         $content.style.display = "flex";
+//         $loading.style.opacity = 0;
+//         setTimeout(() => {
+//             $loading.style.display = "none";
+//         }, 1000);
+//     }, 6000);
+// };
+// toLoginPage();
