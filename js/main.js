21ac056f8ad3434231cf87735f17ae0b3915ce6f
alyfer-pedroso <alyferleandroaraujopedroso@gmail.com>
2023-08-02 18:22:37 -0300

    edit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 (() => {
     const $contentWarning = document.querySelector(".contentWarning");
     const $loading = document.querySelector(".loading");
+    const $login = document.querySelector(".login");
     const $confirmBTN = document.querySelector(".btnConfirm");
+    const $schoolLabel = document.querySelector(".login label:nth-child(4)");
 
     const $selecType = document.getElementById("selecType");
 
@@ -9,7 +11,10 @@
         let $selecTypeValue = $selecType.value;
         console.log($selecTypeValue);
 
-        if ($selecTypeValue != "null") {
+        if ($selecTypeValue == "null") {
+            alert("Por favor, selecione uma opção ");
+        } else if ($selecTypeValue == "student" || $selecTypeValue == "responsible") {
+            $schoolLabel.style.display = "none";
             $loading.style.opacity = 0;
             $confirmBTN.removeEventListener("click", changeToLoading);
             setTimeout(() => {
@@ -18,10 +23,35 @@
                 setTimeout(() => {
                     $loading.style.opacity = 1;
                     $contentWarning.style.display = "none";
+                    setTimeout(() => {
+                        $loading.style.opacity = 0;
+                        $login.style.display = "flex";
+                        setTimeout(() => {
+                            $login.style.opacity = 1;
+                            $loading.style.display = "none";
+                        }, 1000);
+                    }, 3000);
                 }, 1000);
             }, 10);
         } else {
-            alert("Por favor, selecione uma opção ");
+            $loading.style.opacity = 0;
+            $confirmBTN.removeEventListener("click", changeToLoading);
+            setTimeout(() => {
+                $loading.style.display = "flex";
+                $contentWarning.style.opacity = 0;
+                setTimeout(() => {
+                    $loading.style.opacity = 1;
+                    $contentWarning.style.display = "none";
+                    setTimeout(() => {
+                        $loading.style.opacity = 0;
+                        $login.style.display = "flex";
+                        setTimeout(() => {
+                            $login.style.opacity = 1;
+                            $loading.style.display = "none";
+                        }, 1000);
+                    }, 3000);
+                }, 1000);
+            }, 10);
         }
     };
     $confirmBTN.addEventListener("click", changeToLoading);
