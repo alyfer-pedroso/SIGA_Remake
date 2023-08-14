67c7c519135833ebeadd87669a9a98ff841e70a1
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2023-08-14 14:36:07 -0300

    edit

---
+++ js/dados.js
@@ -0,0 +1,9 @@
+function User(first, last, email, phone, rm, ra, course) {
+    this.FIRSTNAME = first;
+    this.LASTNAME = last;
+    this.EMAIL = email;
+    this.PHONE = phone;
+    this.RM = rm;
+    this.RA = ra;
+    this.COURSE = course;
+}
