b5b10d862879864fb7b122fef0d67e1051e25548
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2023-08-14 09:35:38 -0300

    edit

---
+++ js/login.js
@@ -0,0 +1,58 @@
+(() => {
+    const $contentWarning = document.querySelector(".contentWarning");
+    const $loading = document.querySelector(".loading");
+    const $login = document.querySelector(".login");
+    const $confirmBTN = document.querySelector(".btnConfirm");
+    const $schoolLabel = document.querySelector(".login label:nth-child(4)");
+
+    const $selecType = document.getElementById("selecType");
+
+    let changeToLoading = () => {
+        let $selecTypeValue = $selecType.value;
+        console.log($selecTypeValue);
+
+        if ($selecTypeValue == "null") {
+            alert("Por favor, selecione uma opção ");
+        } else if ($selecTypeValue == "student" || $selecTypeValue == "responsible") {
+            $schoolLabel.style.display = "none";
+            $loading.style.opacity = 0;
+            $confirmBTN.removeEventListener("click", changeToLoading);
+            setTimeout(() => {
+                $loading.style.display = "flex";
+                $contentWarning.style.opacity = 0;
+                setTimeout(() => {
+                    $loading.style.opacity = 1;
+                    $contentWarning.style.display = "none";
+                    setTimeout(() => {
+                        $loading.style.opacity = 0;
+                        $login.style.display = "flex";
+                        setTimeout(() => {
+                            $login.style.opacity = 1;
+                            $loading.style.display = "none";
+                        }, 1000);
+                    }, 3000);
+                }, 1000);
+            }, 10);
+        } else {
+            $loading.style.opacity = 0;
+            $confirmBTN.removeEventListener("click", changeToLoading);
+            setTimeout(() => {
+                $loading.style.display = "flex";
+                $contentWarning.style.opacity = 0;
+                setTimeout(() => {
+                    $loading.style.opacity = 1;
+                    $contentWarning.style.display = "none";
+                    setTimeout(() => {
+                        $loading.style.opacity = 0;
+                        $login.style.display = "flex";
+                        setTimeout(() => {
+                            $login.style.opacity = 1;
+                            $loading.style.display = "none";
+                        }, 1000);
+                    }, 3000);
+                }, 1000);
+            }, 10);
+        }
+    };
+    $confirmBTN.addEventListener("click", changeToLoading);
+})();
--- js/main.js
+++ js/main.js
@@ -1,58 +1,22 @@
 (() => {
-    const $contentWarning = document.querySelector(".contentWarning");
-    const $loading = document.querySelector(".loading");
-    const $login = document.querySelector(".login");
-    const $confirmBTN = document.querySelector(".btnConfirm");
-    const $schoolLabel = document.querySelector(".login label:nth-child(4)");
+    const $mainAluno = document.querySelector(".mainAluno");
+    const footerButtons = document.querySelectorAll(".footerAluno button");
 
-    const $selecType = document.getElementById("selecType");
-
-    let changeToLoading = () => {
-        let $selecTypeValue = $selecType.value;
-        console.log($selecTypeValue);
-
-        if ($selecTypeValue == "null") {
-            alert("Por favor, selecione uma opção ");
-        } else if ($selecTypeValue == "student" || $selecTypeValue == "responsible") {
-            $schoolLabel.style.display = "none";
-            $loading.style.opacity = 0;
-            $confirmBTN.removeEventListener("click", changeToLoading);
-            setTimeout(() => {
-                $loading.style.display = "flex";
-                $contentWarning.style.opacity = 0;
-                setTimeout(() => {
-                    $loading.style.opacity = 1;
-                    $contentWarning.style.display = "none";
-                    setTimeout(() => {
-                        $loading.style.opacity = 0;
-                        $login.style.display = "flex";
-                        setTimeout(() => {
-                            $login.style.opacity = 1;
-                            $loading.style.display = "none";
-                        }, 1000);
-                    }, 3000);
-                }, 1000);
-            }, 10);
-        } else {
-            $loading.style.opacity = 0;
-            $confirmBTN.removeEventListener("click", changeToLoading);
-            setTimeout(() => {
-                $loading.style.display = "flex";
-                $contentWarning.style.opacity = 0;
-                setTimeout(() => {
-                    $loading.style.opacity = 1;
-                    $contentWarning.style.display = "none";
-                    setTimeout(() => {
-                        $loading.style.opacity = 0;
-                        $login.style.display = "flex";
-                        setTimeout(() => {
-                            $login.style.opacity = 1;
-                            $loading.style.display = "none";
-                        }, 1000);
-                    }, 3000);
-                }, 1000);
-            }, 10);
+    //Verficando a opção que o usuário está
+    const verifyMain = setInterval(() => {
+        switch ($mainAluno.className) {
+            case "mainAluno HOME": {
+                footerButtons[0].classList.add("markedBTN");
+                break;
+            }
+            default: {
+                var i = 0;
+                while (i < footerButtons.length) {
+                    footerButtons[i].classList.remove("markedBTN");
+                    i++;
+                }
+                break;
+            }
         }
-    };
-    $confirmBTN.addEventListener("click", changeToLoading);
+    }, 10);
 })();
