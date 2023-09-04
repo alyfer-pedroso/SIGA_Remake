5b17d1df535be235d0dcf97470aa17c35094d173
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2023-09-04 09:31:57 -0300

    add allopts

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 (() => {
     const body = document.querySelector("body");
     const $mainAluno = document.querySelector(".mainAluno");
+    const $mainAluno__home = document.getElementById("mainAluno__home");
+    const $mainAluno__allOtp = document.getElementById("mainAluno__allOtp");
+
+    // BTNS
     const footerButtons = document.querySelectorAll(".footerAluno button");
     const $hamburguerMenu = document.querySelector(".aluno-hamburguerMenu");
     const exitButton = document.getElementById("exit");
+    const allOptBtn = document.querySelector(".optionsNav__btn");
 
+    let isNotMenu = false;
+    let isHome = true;
+
+    // Page pre-load
     window.addEventListener("load", () => {
         const loader = document.querySelector(".loading-loader");
         loader.classList.add("lAluno-hidden");
@@ -13,52 +22,87 @@
         });
     });
 
-    $mainAluno.addEventListener("click", () => {
+    if (isHome) {
+        footerButtons.forEach((el) => {
+            el.classList.remove("markedBTN");
+        });
+        footerButtons[0].classList.add("markedBTN");
+        $mainAluno__allOtp.style.display = "none";
+    }
+
+    function loaderPage() {
+        loader = document.createElement("div");
+        loader.classList = "loading-loader lAluno";
+        body.appendChild(loader);
+        setTimeout(() => {
+            loader.classList.add("lAluno-hidden");
+        }, 75);
+        loader.addEventListener("transitionend", () => {
+            loader.remove();
+        });
+    }
+
+    // Funções das páginas
+    function initPage() {
+        location.reload();
+    }
+
+    function accessAllOptions() {
+        $mainAluno__home.style.display = "none";
+        $mainAluno__allOtp.style.display = "block";
         $hamburguerMenu.classList.remove("opened");
-        $mainAluno.classList.remove("MENU");
-        $mainAluno.classList.add("HOME");
-    });
+        footerButtons.forEach((el) => {
+            el.classList.remove("markedBTN");
+        });
+        isNotMenu = !isNotMenu;
+        isHome = false;
+        loaderPage();
+        scrollTo(0, 0);
+    }
+
+    // Adciocionando eventos nos botoes
+    allOptBtn.addEventListener("click", accessAllOptions);
+
     footerButtons[0].addEventListener("click", () => {
-        $hamburguerMenu.classList.remove("opened");
-        $mainAluno.classList.remove("MENU");
-        $mainAluno.classList.add("HOME");
+        if (isNotMenu) {
+            initPage();
+            scrollTo(0, 0);
+        } else {
+            if ($hamburguerMenu.classList == "aluno-hamburguerMenu opened") {
+                footerButtons[0].classList.add("markedBTN");
+                footerButtons.forEach((el) => {
+                    el.classList.remove("markedBTN");
+                });
+                body.style.overflow = "visible";
+            }
+            $hamburguerMenu.classList.remove("opened");
+            footerButtons[0].classList.add("markedBTN");
+        }
     });
+
     footerButtons[1].addEventListener("click", () => {
         $hamburguerMenu.classList.toggle("opened");
-        $mainAluno.classList.toggle("MENU");
-        $mainAluno.classList.toggle("HOME");
+        if ($hamburguerMenu.classList == "aluno-hamburguerMenu opened") {
+            footerButtons.forEach((el) => {
+                el.classList.remove("markedBTN");
+            });
+            footerButtons[1].classList.add("markedBTN");
+        } else {
+            footerButtons.forEach((el) => {
+                el.classList.remove("markedBTN");
+            });
+            if (isHome) {
+                footerButtons.forEach((el) => {
+                    el.classList.remove("markedBTN");
+                });
+                footerButtons[0].classList.add("markedBTN");
+            }
+            body.style.overflow = "visible";
+        }
     });
 
     exitButton.addEventListener("click", () => {
         localStorage.clear();
         open("../index.html", "_self");
     });
-
-    //Verficando a opção que o usuário está
-    const verifyMain = setInterval(() => {
-        switch ($mainAluno.className) {
-            case "mainAluno HOME": {
-                footerButtons[1].classList.remove("markedBTN");
-                footerButtons[0].classList.add("markedBTN");
-                body.style.overflow = "visible";
-                break;
-            }
-
-            case "mainAluno MENU": {
-                footerButtons[1].classList.add("markedBTN");
-                footerButtons[0].classList.remove("markedBTN");
-                body.style.overflow = "hidden";
-                break;
-            }
-
-            default: {
-                var i = 0;
-                while (i < footerButtons.length) {
-                    footerButtons[i].classList.remove("markedBTN");
-                    i++;
-                }
-                break;
-            }
-        }
-    }, 10);
 })();
